931f8f3f7a2d00b6df5163800180d7ff0e6a8218
DESKTOP-DTFO15Q\Akiba <kredynets.a@gmail.com>
2022-10-03 04:10:42 +0300

    gitignore-fix

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get('mogoURL');
+const db = config.get('mongoURL');
 
 const connectDB = async () => {
     try {
